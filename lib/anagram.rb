11a311b250500aff516c27c1f3bf32c424a020f3
Warda Ayaz <wayaz107@gmail.com>
2020-07-29 07:10:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ def initialize(word)
 end
 
 def match(array_of_words)
-array_of_words.select do |element| ()@word.split("").sort) == (element.split("").sort)
+array_of_words.select do |element| (@word.split("").sort) == (element.split("").sort)
 end
 end
 
