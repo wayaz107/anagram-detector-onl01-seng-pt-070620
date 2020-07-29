512036ab1ac09c8fb987e0c82c38eba4c4f34055
Warda Ayaz <wayaz107@gmail.com>
2020-07-29 07:10:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,14 +3,14 @@ class Anagram
 
 attr_accessor :word
 
-def initialize
+def initialize(word)
   @word = word
 end
 
-def match
+def match(array_of_words)
+array_of_words.select do |element| ()@word.split("").sort) == (element.split("").sort)
 end
-
-
+end
 
 
 
